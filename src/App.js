1c30e85b3caff5d27f5a6649fc3ec8574a30abc6
wazimu <hesbonosoro1@gmail.com>
2021-10-21 00:35:46 +0300

    configured Google Analytics in index.html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css';
 import Header from './components/Header'
 import DatingCards from './components/DatingCards'
 import SwipeButtons from './components/SwipeButtons'
+import ReactGA from "react-ga";
+
+ReactGA.initialize('G-7LFYFDYDX4');
+
 function App() {
+  useEffect(() => {
+      ReactGA.pageview('/');
+  })
   return (
     <div className="app">
       <Header />
